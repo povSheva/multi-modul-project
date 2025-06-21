6514a7093c51c61ab5cc73cd38f858c2a0359e3b
SheWick <daniilmakeenko730@gmail.com>
2025-06-21 03:47:12 +0300

    Initial commit

---
+++ service_b/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("Jon", 30);
+        System.out.println("User data: " + user);
+    }
+}
+
